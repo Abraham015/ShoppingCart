090fb355640ed01cd9c9ed73128994a781e0fcc5
AbrahamH015 <79383720+Abraham015@users.noreply.github.com>
2024-08-29 09:35:08 -0600

    First commit with structure of the project

---
+++ dream-shops/src/test/java/dev/abraham/dreamshops/DreamShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abraham.dreamshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
